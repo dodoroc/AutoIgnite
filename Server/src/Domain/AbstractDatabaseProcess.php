c9d7fd7993a281fc6279c42a33261c9d35c58d14
dodoroc <dodoroc@gmail.com>
2024-08-27 13:45:25 -0400

    update db cred source

--- Server/src/Domain/AbstractDatabaseProcess.php
+++ Server/src/Domain/AbstractDatabaseProcess.php
@@ -23,9 +23,9 @@ abstract class AbstractDatabaseProcess extends AbstractProcess
     $usr = CFG['DB_USER'];
     $pwd = CFG['DB_PASS'];
 
-    $dsn = "pgsql:dbname=projects;options='--client_encoding=UTF8'";
-    $usr = "projects_user";
-    $pwd = "655321";
+    // $dsn = "pgsql:dbname=projects;options='--client_encoding=UTF8'";
+    // $usr = "projects_user";
+    // $pwd = "655321";
 
     $this->dbc = new PDO($dsn, $usr, $pwd, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
