791bdaa4c69da42f95427a7d2b0de09cce7af343
dodoroc <dodoroc@gmail.com>
2024-09-02 13:02:38 -0400

    -

--- Server/src/Entity/Episode.php
+++ Server/src/Entity/Episode.php
@@ -11,8 +11,7 @@ final class Episode extends AbstractEntity
   public function __construct(
     public readonly SeriesId $seriesId,
     public readonly string $name,
-    public readonly string|null $airDate,
-    public readonly ProgramId $programId,
+    public readonly string|null $airDate
   ) {}
 
   public function isValid() : bool
