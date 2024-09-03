3a119fd49290a728fa3de86092a7a9c9358afa52
dodoroc <dodoroc@gmail.com>
2024-09-03 12:30:10 -0400

    -

--- Site/public/assets/app.mjs
+++ Site/public/assets/app.mjs
@@ -15,12 +15,16 @@ const store = reactive({
   programs: null,
 });
 
+async function loadSeries() {
+  return await fetch('http://192.168.50.200:9080/series')
+  .then(data => store.series = data.json());
+}
 // manipulate it here
 // store.inc()
 function seriesChanged(itm) {
   console.dir(itm);
 }
-
+loadSeries();
 createApp({
   // share it with app scopes
   store
