ed9c06030a7ced79a3249aac28d83e089f248a03
dodoroc <dodoroc@gmail.com>
2024-09-06 14:35:52 -0400

    -

--- Server/src/App.php
+++ Server/src/App.php
@@ -36,6 +36,8 @@ final class App
 
     // CHECK Which server var is most correct (SCRIPT_NAME PHP_SELF apache/php)(DOCUMENT_URI nginx)
     // TEST Confirm UTF-8 isn't mangled in any way
+    // There is a bug in filter_input with INPUT_SERVER
+    $path = filter_var($_SERVER['DOCUMENT_URI'], FILTER_SANITIZE_URL); //
     $path = trim(urldecode($path ?: ''), '/');
     $path = $path === '' ? [] : explode('/', $path);
 
