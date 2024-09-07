b62b1350bf919f244a54c284af9b5b4be047a6d2
dodoroc <dodoroc@gmail.com>
2024-09-07 10:38:24 -0400

    -

--- Site/public/assets/app.mjs
+++ Site/public/assets/app.mjs
@@ -2,7 +2,13 @@ import { createApp, reactive } from 'https://unpkg.com/petite-vue@0.4.1/dist/pet
 
 function sel() {
   const el = document.querySelector('select');
-  console.log('sel val', el.value, el.selectedIndex, el.options[el.selectedIndex].text, el.options[el.selectedIndex].textContent, el.selectedOptions[0].value);
+  console.log('sel val',
+    el.value,
+    el.selectedIndex,
+    el.options[el.selectedIndex].text,
+    el.options[el.selectedIndex].textContent,
+    el.selectedOptions[0].value
+  );
 }
 
 const store = reactive({
