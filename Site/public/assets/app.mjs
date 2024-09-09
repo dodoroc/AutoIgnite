2d841b134cd502de33520bc4bd3f4dd2531e2990
dodoroc <dodoroc@gmail.com>
2024-09-09 17:33:48 -0400

    -

--- Site/public/assets/app.mjs
+++ Site/public/assets/app.mjs
@@ -2,7 +2,6 @@ import { createApp, reactive } from 'https://unpkg.com/petite-vue@0.4.1/dist/pet
 
 const appStart_ms = Date.now();
 
-// const source = {};
 const model = reactive({
   source: {
     series: [],
