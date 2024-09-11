141dab4e71569668bc6a0e1c9f50516daccfe2ea
dodoroc <dodoroc@gmail.com>
2024-09-11 01:11:20 -0400

    -

--- Site/public/assets/app.mjs
+++ Site/public/assets/app.mjs
@@ -62,8 +62,9 @@ const model = reactive({
 
       // if (this.actions.length) res = this.actions[0](res);
       // console.dir(this.actions);
-      const f = arr => Array.prototype.filter.bind(arr, o => !!!o.watchedOn);
-      res = f(res)();
+      // const f = arr => Array.prototype.filter.bind(arr, o => !!!o.watchedOn);
+      // res = f(res)();
+      res = res.filter(o => !o.watchedOn);
 
       for (const fnc in this.actions) {
         // console.dir(fnc);
