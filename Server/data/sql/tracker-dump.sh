00d3f667da575ad070aa70e000e3c238e3a3cf34
dodoroc <dodoroc@gmail.com>
2024-09-28 23:47:41 -0400

    -

--- Server/data/sql/tracker-dump.sh
+++ Server/data/sql/tracker-dump.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 
-pg_dump projects --schema=tracker --schema-only -h localhost > ~/tracker-schema.sql
-pg_dump projects --schema=tracker --data-only -h localhost > ~/tracker-data.sql
+#pg_dump projects --schema=tracker --schema-only -h localhost > ~/tracker-schema.sql
+#pg_dump projects --schema=tracker --data-only -h localhost > ~/tracker-data.sql
+pg_dump projects --schema=tracker --schema-only > ~/tracker-schema.sql
+pg_dump projects --schema=tracker --data-only > ~/tracker-data.sql
 
