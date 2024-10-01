a6a4429f0f508ad2a31a7676b01455591b131f75
dodoroc <dodoroc@gmail.com>
2024-10-01 14:01:45 -0400

    -

--- Site/public/assets/result.mjs
+++ Site/public/assets/result.mjs
@@ -1,24 +1,19 @@
-let values = [];
-let zz = 42;
 
-export default {
-  // values: [],
-
+export result {
+  values: [],
+/*
   get values() {
     return this.values;
   },
   set values(v) {
     this.values = v;
   },
-
+*/
   set source(src) {
-    // this.values = src;
     this.values = src;
   },
 
   get length() {
-    // console.log(zz, this.zz, globalThis.zz, window.zz, self.zz);
-    // return this.values.length;
     return this.values.length;
   },
 }
