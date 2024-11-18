50a336e91e56e4a9ed01700ceda99e29c91c82ff
dodoroc <dodoroc@gmail.com>
2024-11-17 20:40:19 -0500

    -

--- Server/src/Entity/EntityInterface.php
+++ Server/src/Entity/EntityInterface.php
@@ -9,10 +9,5 @@ namespace Server\Entity;
 interface EntityInterface
 {
   // Check for validity of fields that are mapped from db
-  // if valid return true
-  // else return array of failed property names
-  // public function isValid() : array|bool;
-
-  // As above but bool val on success/failure
   public function isValid() : bool;
 }
