138bf1026d3c1336430a1551a2451563c2778915
dodoroc <dodoroc@gmail.com>
2024-11-18 17:47:38 -0500

    -

--- Server/src/Response/ResponseInterface.php
+++ Server/src/Response/ResponseInterface.php
@@ -9,6 +9,6 @@ namespace Server\Response;
 
 interface ResponseInterface
 {
-  public function setHeader(string $key, string|array|null $value, bool $replace = true) : void;
+  // public function setHeader(string $key, string|array|null $value, bool $replace = true) : void;
   public function vent() : void;
 }
