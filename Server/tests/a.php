d486d2e48f9760153212fe11943d2aab5ddf1318
dodoroc <dodoroc@gmail.com>
2024-11-19 22:11:23 -0500

    -

---
+++ Server/tests/a.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+/**
+ *
+ */
+namespace Test;
+
+require '../vendor/autoload.php';
+
+
+echo "\n";
+echo "\nini ", time();
+
+$fibers = [];
+for ($i = 0; $i < 3; $i++) {
+  $fibers[] = new Fiber(function () {
+    echo "\n pro $i ",time();
+    sleep(30+$i);
+    echo "\n epi $i ",time();
+    return;
+  });
+}
+
+echo "\nmid ", time();
+
+$results = [];
+
+foreach ($fibers as $fiber) {
+  $result = $fiber->start();
+  $results[] = $result;
+}
+
+echo "\nfin ", time();
+echo "\n";
